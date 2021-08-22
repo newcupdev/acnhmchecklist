8c9caac69eb8bd7d71b8a6622fa933af6e675dd5
Angelo <acnucup0610@gmail.com>
2021-08-22 10:51:07 -0700

    save date component done

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { Card, Tile, Button, Icon } from 'react-native-elements';
 import { baseUrl } from '../shared/baseUrl';
 import * as MailComposer from 'expo-mail-composer';
@@ -20,38 +20,114 @@ class Contact extends Component {
 
     render() {
         return (
-            <ScrollView>
-                <Tile
-                    imageSrc={{uri: baseUrl + 'images/isabelle_contact.png'}}
-                    featured
-                />
-                <Card
-                    title="Do you have any feedback?"
-                    wrapperStyle={{margin: 20}}>
+            <ImageBackground
+                source={{uri: baseUrl + 'images/leaf_icon_bg.png'}}
+                resizeMode="cover"
+                style={styles.image}
+            >
+                <ScrollView>
+                    <View style={{margin: 5}}>
+                        <View style={{margin: 5, backgroundColor: '#F4A460', borderRadius: 20}}>
+                            <View style={{flex:1, margin: 20, overflow: 'hidden', borderRadius: 20}}>
+                                <Tile
+                                    imageSrc={{uri: baseUrl + 'images/isabelle_contact.png'}}
+                                    featured
+                                    imageProps={{resizeMode: 'cover'}}
+                                    containerStyle={{
+                                        borderRadius: 20,
+                                        overflow: 'hidden'
+                                    }}
+                                />
+                            </View>
 
-                <Text>Please click the email icon below to email us for any issues like: {'\n'}</Text>
-                <Text>{'\t'}Misinformation</Text>
-                <Text>{'\t'}Application error</Text>
-                <Text>{'\t'}Server issues</Text>
+                            <View style={{flex:1, marginBottom: 20, marginTop: -10}}>
+                                <Card
+                                    title="Do you have any feedback?"
+                                    titleStyle={{
+                                        fontFamily: 'Fink-Heavy',
+                                        fontWeight: 'normal',
+                                        fontSize: 25
+                                    }}
+                                    containerStyle={{
+                                        backgroundColor: '#FFE4B5', 
+                                        margin: 20,
+                                        borderRadius: 20,
+                                        overflow: 'hidden',
+                                    }}
+                                    >
 
-                
-                <Button
-                        title="Send Email"
-                        buttonStyle={{backgroundColor: '#F4A460', margin: 40}}
-                        icon={<Icon
-                            name='envelope-o'
-                            type='font-awesome'
-                            color='#fff'
-                            iconStyle={{marginRight: 10}}
-                        />}
-                        onPress={() => this.sendMail()}
-                    />
+                                    <View style={styles.viewContainer}>
+                                        <Text style={styles.viewLabelText}>
+                                            Please click the email icon below to email us for any issues like:
+                                        </Text>
+                                        
+                                        <View style={styles.textContainer}>
+                                            <Text style={styles.textInfo}>Misinformation</Text>
+                                            <Text style={styles.textInfo}>Application Error</Text>
+                                            <Text style={styles.textInfo}>Server Issues</Text>
+                                        </View>
+                                    </View>
 
-                </Card>
-
-            </ScrollView>
+                                    <View style={{alignItems: 'center'}}>
+                                        <Button
+                                            title="Send Email"
+                                            titleStyle={{
+                                                fontFamily: 'Fink-Heavy',
+                                                fontSize: 25,
+                                                justifyContent: 'center'
+                                            }}
+                                            buttonStyle={{backgroundColor: '#8B4513', margin: 20, padding: 10, borderRadius: 20}}
+                                            icon={<Icon
+                                                name='envelope-open-text'
+                                                type='font-awesome-5'
+                                                color='#fff'
+                                                size={30}
+                                                iconStyle={{marginRight: 10, justifyContent:'center'}}
+                                            />}
+                                            onPress={() => this.sendMail()}
+                                        />
+                                    </View>
+                                </Card>
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
+            </ImageBackground>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    
+    image: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    viewLabelText: {
+        fontFamily: 'Fink-Heavy',
+        fontSize: 18,
+        margin: 5
+    },
+    viewContainer: {
+        backgroundColor: '#DEB887', 
+        margin: 3,
+        padding: 10, 
+        borderRadius: 10, 
+        overflow: 'hidden'
+    },
+    textContainer: {
+        backgroundColor: '#FFEFD5', 
+        flex: 3, 
+        margin: 5,
+        borderRadius: 7,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textInfo: {
+        fontFamily: 'Varela-Round',
+        fontSize: 15,
+        margin: 5
+    }
+});
 
 export default Contact;
--- components/SaveDateComponent.js
+++ components/SaveDateComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text, StyleSheet, Alert, Picker, Switch, Button } from 'react-native';
-import { Tile } from 'react-native-elements';
+import { View, ScrollView, Text, StyleSheet, Alert, Picker, Switch, Image } from 'react-native';
+import { Button } from 'react-native-elements';
 import { baseUrl } from '../shared/baseUrl';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Notifications from 'expo-notifications';
@@ -62,101 +62,148 @@ class SaveDate extends Component {
     render() {
         return (
             <ScrollView style={{backgroundColor: '#F4A460'}}>
-                <Tile
-                    imageSrc={{uri: baseUrl + 'images/bugoff.jpg'}}
-                    featured
-                />
-                <View style={styles.container}>
-                
-                    <View style={styles.formRow}>
-                            <Text style={styles.formLabel}>Event</Text>
-                            <Picker 
-                                style={styles.formItem}
-                                selectedValue={this.state.dayEvent}
-                                onValueChange={itemValue => this.setState({dayEvent: itemValue})}
-                            >
-                                <Picker.Item label='Bug off' value='Bug off' />
-                                <Picker.Item label='Fish tourney' value='Fish tourney' />
-                                
-                            </Picker>
-                        </View>
-                        <View style={styles.formRow}>
-                            <Text style={styles.formLabel}>My island?</Text>
-                            <Switch
-                                style={styles.formItem}
-                                value={this.state.myIsland}
-                                trackColor={{true: '#5637DD', false: null}}
-                                onValueChange={value => this.setState({myIsland: value})}
-                            />
-                        </View>
-                        <View style={styles.formRow}>
-                            <Text style={styles.formLabel}>Date</Text>
-                            <Button
-                                onPress={() =>
-                                    this.setState({showCalendar: !this.state.showCalendar})
-                                }
-                                title={this.state.date.toLocaleDateString('en-US')}
-                                color='#5637DD'
-                                accessibilityLabel='Tap me to save the date'
-                            />
-                        </View>
-                        {this.state.showCalendar && (
-                            <DateTimePicker
-                                value={this.state.date}
-                                mode={'date'}
-                                display='default'
-                                onChange={(event, selectedDate) => {
-                                    selectedDate && this.setState({date: selectedDate, showCalendar: false});
-                                }}
-                                style={styles.formItem}
-                            />
-                        )}
-                        <View style={styles.formRow}>
-                            <Button
-                                
-                                title='Save'
-                                color='#5637DD'
-                                accessibilityLabel='Tap me to save the date'
 
-                                onPress={() =>
-                                    Alert.alert(
-                                        'Begin Saving',
-                                        'Event of the day: ' + this.state.dayEvent + '\n \n' +
-                                        'The event will be ' + (this.state.myIsland ? 'in my island' : 'in my friend\'s island') + '\n \n' +
-                                        'Date: ' + this.state.date.toLocaleDateString('en-US'),
-                                        [
-                                            {
-                                                text: 'Cancel',
-                                                onPress: () => this.resetForm(),
-                                                style: 'cancel'
-                                            },
-                                            {
-                                                text: 'OK',
-                                                onPress: () => {
-                                                    this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'), this.state.dayEvent, this.state.myIsland);
-                                                    this.resetForm();
-                                                }
-                                            },
-                                        ],
-                                        { cancelable: false }
-                                    )
-                                }
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+
+                    <View style={styles.imageContainer}>
+                        <Image 
+                            style={{resizeMode: 'cover', width: 500, height: 330}}
+                            source={{uri: baseUrl + 'images/bugoff.jpg'}} 
+                            
                             />
-                        </View>
+                        
+                    </View>
+
+                    <View style={styles.imageContainer}>
+                        <Image 
+                            style={{resizeMode: 'cover', width: 500, height: 330}}
+                            source={{uri: baseUrl + 'images/fishtourney.png'}} 
+                            />
+                    </View>
+
+                    <View style={styles.formViewContainer}>
+
+                        
+                    
+                            <View style={styles.formRow}>
+                                <Text style={styles.formLabel}>Event</Text>
+                                <Picker 
+                                    style={styles.formItem}
+                                    selectedValue={this.state.dayEvent}
+                                    onValueChange={itemValue => this.setState({dayEvent: itemValue})}
+                                >
+                                    <Picker.Item label='Bug off' value='Bug off' />
+                                    <Picker.Item label='Fish tourney' value='Fish tourney' />
+                                    
+                                </Picker>
+                            </View>
+    
+                            <View style={styles.formRow}>
+                                <Text style={styles.formLabel}>My island?</Text>
+                                <Switch
+                                    style={styles.formItem}
+                                    value={this.state.myIsland}
+                                    trackColor={{true: '#F4A460', false: null}}
+                                    onValueChange={value => this.setState({myIsland: value})}
+                                />
+                            </View>
+
+                            <View style={styles.formRow}>
+                                <Text style={styles.formLabel}>Date</Text>
+                                <Button
+                                    onPress={() =>
+                                        this.setState({showCalendar: !this.state.showCalendar})
+                                    }
+                                    title={this.state.date.toLocaleDateString('en-US')}
+                                    titleStyle={{
+                                        fontFamily: 'Varela-Round'
+                                    }}
+                                    
+                                    accessibilityLabel='Tap me to save the date'
+                                    buttonStyle={{
+                                        borderRadius: 20,
+                                        backgroundColor: '#F4A460',
+                                        margin: 5
+                                    }}
+                                    
+                                />
+                            </View>
+    
+                            {this.state.showCalendar && (
+                                <DateTimePicker
+                                    value={this.state.date}
+                                    mode={'date'}
+                                    display='default'
+                                    onChange={(event, selectedDate) => {
+                                        selectedDate && this.setState({date: selectedDate, showCalendar: false});
+                                    }}
+                                    style={styles.formItem}
+                                />
+                            )}
+    
+                            <View style={styles.formRow}>
+                                <Button
+                                    
+                                    title='Save'
+                                    titleStyle={{
+                                        fontFamily: 'Varela-Round'
+                                    }}
+                                    buttonStyle={{
+                                        borderRadius: 20,
+                                        backgroundColor: '#F4A460',
+                                        margin: 5,
+                                        width: 80
+                                    }}
+                                    accessibilityLabel='Tap me to save the date'
+                
+                                    onPress={() =>
+                                        Alert.alert(
+                                            'Begin Saving',
+                                            'Event of the day: ' + this.state.dayEvent + '\n \n' +
+                                            'The event will be ' + (this.state.myIsland ? 'in my island' : 'in my friend\'s island') + '\n \n' +
+                                            'Date: ' + this.state.date.toLocaleDateString('en-US'),
+                                            [
+                                                {
+                                                    text: 'Cancel',
+                                                    onPress: () => this.resetForm(),
+                                                    style: 'cancel'
+                                                },
+                                                {
+                                                    text: 'OK',
+                                                    onPress: () => {
+                                                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'), this.state.dayEvent, this.state.myIsland);
+                                                        this.resetForm();
+                                                    }
+                                                },
+                                            ],
+                                            { cancelable: false }
+                                        )
+                                    }
+                                />
+                            </View>
+                        
+                    </View>
                 </View>
-                <Tile
-                    imageSrc={{uri: baseUrl + 'images/fishtourney.png'}}
-                    featured
-                />
+
+                
+                
+                
+                
+
             </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    imageContainer: {
+        flex: 1, 
+        justifyContent: 'center',
+        flexWrap: 'wrap'
+    },
     container: {
         justifyContent: 'center',
-        margin: 10,
+        margin: 30,
         alignItems: 'center',
         
     },
@@ -172,11 +219,24 @@ const styles = StyleSheet.create({
         margin: 5,
     },
     formLabel: {
-        fontSize: 18,
+        fontSize: 20,
+        fontFamily: 'Fink-Heavy',
         flex: 2
     },
     formItem: {
-        flex: 1
+        flex: 3
+    },
+    formViewContainer: {
+        margin: 30,
+        padding: 20, 
+        backgroundColor: 'rgba(255,248,220,0.75)', 
+        position: 'absolute', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        width: 350,
+        height: 200,
+        borderRadius: 20
+        
     }
 });
 
