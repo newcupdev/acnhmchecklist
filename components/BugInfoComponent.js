77955adaeb6a0194eabf9a633440e2779e8a8766
Angelo <acnucup0610@gmail.com>
2021-08-21 20:37:00 -0700

    info layout

--- components/BugInfoComponent.js
+++ components/BugInfoComponent.js
@@ -65,153 +65,81 @@ function RenderBug(props) {
 
                         <View style={{margin: 5}}>
                             
-                            <View style={{
-                                backgroundColor: 'gray', 
-                                flex: 1, 
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'}}>
+                            <View style={styles.viewContainer}>
 
-                                <Text>Region Availability:</Text>
+                                <Text style={styles.viewLabelText}>Region Availability:</Text>
 
                                 <View style={{flexDirection: 'row'}}>
-                                    <View style={{backgroundColor: 'green', flex: 1, margin: 5}}>
-                                        <Text>North</Text>
+                                    <View style={{backgroundColor: '#FFEFD5', flex: 1, margin: 5}}>
+                                        <Text style={styles.viewLabelText}>North:</Text>
                                     </View>
-                                    <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                    <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                         <Text>{bug.northavailability}</Text>
                                     </View>
                                 </View>
 
                                 <View style={{flexDirection: 'row'}}>
-                                    <View style={{backgroundColor: 'green', flex: 1, margin: 5}}>
-                                        <Text>South</Text>
+                                    <View style={{backgroundColor: '#FFEFD5', flex: 1, margin: 5}}>
+                                        <Text style={styles.viewLabelText}>South:</Text>
                                     </View>
-                                    <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                    <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                         <Text>{bug.southavailability}</Text>
                                     </View>
                                 </View>
 
                             </View>
                             
-                            <View style={{
-                                backgroundColor: 'gray',
-                                flexDirection: 'row',
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'
-                                }}>
-                                <View style={{backgroundColor: 'green', flex: 1, margin: 5}}>
-                                    <Text>Location</Text>
+                            <View style={[styles.viewContainer, {flexDirection: 'row'}]}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 2, margin: 5}}>
+                                    <Text style={styles.viewLabelText}>Location: </Text>
                                 </View>
-                                <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                     <Text>{bug.location}</Text>
                                 </View>
                             </View>
 
-                            <View style={{
-                                backgroundColor: 'gray',
-                                flexDirection: 'row',
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'
-                                }}>
-                                <View style={{backgroundColor: 'green', flex: 1, margin: 5}}>
-                                    <Text>Rarity</Text>
+                            <View style={[styles.viewContainer, {flexDirection: 'row'}]}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 1, margin: 5}}>
+                                    <Text style={styles.viewLabelText}>Rarity:</Text>
                                 </View>
-                                <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                     <Text>{bug.rarity}</Text>
                                 </View>
                             </View>
 
-                            <View style={{
-                                backgroundColor: 'gray',
-                                flexDirection: 'row', 
-                                flex: 1, 
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'
-                                
-                                }}>
-                                <View style={{backgroundColor: 'green', flex: 1, margin: 5}}>
-                                    <Text>Price</Text>
+                            <View style={[styles.viewContainer, {flexDirection: 'row'}]}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 1, margin: 5}}>
+                                    <Text style={styles.viewLabelText}>Price: </Text>
                                 </View>
-                                <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                     <Text>{bug.price}</Text>
                                 </View>
                             </View>
 
-                            <View style={{
-                                backgroundColor: 'gray', 
-                                flexDirection: 'row',
-                                flex: 1, 
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'
-                                
-                                }}>
-                                <View style={{backgroundColor: 'green', flex: 1, margin: 5}}>
-                                    <Text>Flick Price</Text>
+                            <View style={[styles.viewContainer, {flexDirection: 'row'}]}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 1, margin: 5}}>
+                                    <Text style={styles.viewLabelText}>Price by Flick: </Text>
                                 </View>
-                                <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                     <Text>{bug.flickprice}</Text>
                                 </View>
                             </View>
 
-                            <View style={{
-                                backgroundColor: 'gray', 
-                                flex: 1, 
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'
-                                }}>
-                                <Text>Your catchphrase</Text>
-                                <View style={{backgroundColor: 'blue', flex: 2, margin: 5}}>
+                            <View style={styles.viewContainer}>
+                                <Text style={styles.viewLabelText}>Your catchphrase:</Text>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 2, margin: 5}}>
                                     <Text>{bug.catchphrase}</Text>
                                 </View>
                             </View>
 
-                            <View style={{
-                                backgroundColor: 'gray', 
-                                flex: 1, 
-                                margin: 3,
-                                padding: 10, 
-                                borderRadius: 10, 
-                                overflow: 'hidden'
-                                }}>
-                                <Text>Blather's Catchphrase</Text>
+                            <View style={styles.viewContainer}>
+                                <Text style={styles.viewLabelText}>Blather's Catchphrase:</Text>
                                 
-                                <View style={{backgroundColor: 'blue', flex: 3, margin: 5}}>
+                                <View style={{backgroundColor: '#FFEFD5', flex: 3, margin: 5}}>
                                     <Text>{bug.museumphrase}</Text>
                                 </View>
                             </View>
                         </View>
-                        
-                        {/* <View style={{borderColor: '#000', margin: 10}}>
-                            <Text style={styles.textLabel}>Region Availability (North/South): </Text>
-                        </View>
-                        
-                        <Text><Text style={styles.textLabel}>N: </Text>{bug.northavailability}</Text>
-                        <Text><Text style={styles.textLabel}>S: </Text>{bug.southavailability}</Text>
-
-
-                        <Text><Text style={styles.textLabel}>Time: </Text> {bug.time}</Text>
-                        <Text><Text style={styles.textLabel}>Location: </Text> {bug.location}</Text>
-                        <Text><Text style={styles.textLabel}>Rarity: </Text> {bug.rarity}</Text>
-                        <Text><Text style={styles.textLabel}>Price: </Text> {bug.price} bells</Text>
-                        <Text><Text style={styles.textLabel}>Flick price: </Text> {bug.flickprice} bells</Text>
-
-                        <Text style={styles.textLabel}>Your catchphrase: </Text>
-                        <Text>{'\t'}{bug.catchphrase}</Text>
-
-                        <Text style={styles.textLabel}>Blather's catchphrase: </Text>
-                        <Text style={{marginBottom: 20}}>{'\t'}{bug.museumphrase}</Text> */}
 
                         <View style={{alignItems: 'center'}}>
                             <Icon
@@ -226,93 +154,6 @@ function RenderBug(props) {
                             />
                         </View>
                     </Card>
-
-                    {/* <View style={{
-                        margin: 10,
-                        borderRadius: 20,
-                        overflow: 'hidden'
-                    }}>
-                        <View style={{backgroundColor: 'red', borderColor: '#000', borderWidth: 2, borderRadius: 20, padding: 10}}>
-                            <Text style={{textAlign: 'center', fontFamily: 'Fink-Heavy', fontSize: 25}}>{bug.name}</Text>
-                        </View>
-                        <View>
-                            <Text>test</Text>
-                        </View>
-                    </View> */}
-                    <View style={{margin: 10}}>
-                        <View style={{backgroundColor: 'lightgray', flex: 1, alignItems: 'center'}}>
-                            <Text>{bug.name}</Text>
-                        </View>
-                        <View style={{backgroundColor: 'gray', flex: 1}}>
-                            <Text>Region Availability:</Text>
-                            <View style={{flexDirection: 'row'}}>
-                                <View style={{backgroundColor: 'green', flex: 1}}>
-                                    <Text>North</Text>
-                                </View>
-                                <View style={{backgroundColor: 'blue', flex: 3}}>
-                                    <Text>North Desc</Text>
-                                </View>
-                            </View>
-                            <View style={{flexDirection: 'row'}}>
-                                <View style={{backgroundColor: 'green', flex: 1}}>
-                                    <Text>South</Text>
-                                </View>
-                                <View style={{backgroundColor: 'blue', flex: 3}}>
-                                    <Text>South Desc</Text>
-                                </View>
-                            </View>
-                        </View>
-                        
-                        <View style={{flexDirection: 'row'}}>
-                            <View style={{backgroundColor: 'green', flex: 1}}>
-                                <Text>Location</Text>
-                            </View>
-                            <View style={{backgroundColor: 'blue', flex: 3}}>
-                                <Text>Location Desc</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection: 'row'}}>
-                            <View style={{backgroundColor: 'green', flex: 1}}>
-                                <Text>Rarity</Text>
-                            </View>
-                            <View style={{backgroundColor: 'blue', flex: 3}}>
-                                <Text>Rarity Desc</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection: 'row'}}>
-                            <View style={{backgroundColor: 'green', flex: 1}}>
-                                <Text>Price</Text>
-                            </View>
-                            <View style={{backgroundColor: 'blue', flex: 3}}>
-                                <Text>Price Desc</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection: 'row'}}>
-                            <View style={{backgroundColor: 'green', flex: 1}}>
-                                <Text>Flick Price</Text>
-                            </View>
-                            <View style={{backgroundColor: 'blue', flex: 3}}>
-                                <Text>Flick Price Desc</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection: 'column'}}>
-                            <View style={{backgroundColor: 'green', flex: 1}}>
-                                <Text>Catchphrase</Text>
-                            </View>
-                            <View style={{backgroundColor: 'blue', flex: 2}}>
-                                <Text>Your catchphrase</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection: 'column'}}>
-                            <View style={{backgroundColor: 'green', flex: 1}}>
-                                <Text>Blathers Catchphrase</Text>
-                            </View>
-                            <View style={{backgroundColor: 'blue', flex: 3}}>
-                                <Text>Blatherss catchphrase</Text>
-                            </View>
-                        </View>
-                    </View>
-
                 </ScrollView>
             </ImageBackground>
         );
@@ -350,6 +191,18 @@ const styles = StyleSheet.create({
     image: {
         flex: 1,
         justifyContent: "center"
+    },
+    viewContainer: {
+        backgroundColor: '#F4A460', 
+        margin: 3,
+        padding: 10, 
+        borderRadius: 10, 
+        overflow: 'hidden'
+    },
+    viewLabelText: {
+        fontFamily: 'Fink-Heavy',
+        fontSize: 18,
+        margin: 5
     }
 });
 
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -29,11 +29,91 @@ class Directory extends Component {
         };
     
         return (
-            <FlatList
-                data={this.state.fishes}
-                renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
-            />
+            <View>
+                <FlatList
+                    data={this.state.fishes}
+                    renderItem={renderDirectoryItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+
+
+<View style={{margin: 10}}>
+                        <View style={{backgroundColor: 'lightgray', flex: 1, alignItems: 'center'}}>
+                            <Text>{bug.name}</Text>
+                        </View>
+                        <View style={{backgroundColor: 'gray', flex: 1}}>
+                            <Text>Region Availability:</Text>
+                            <View style={{flexDirection: 'row'}}>
+                                <View style={{backgroundColor: 'green', flex: 1}}>
+                                    <Text>North</Text>
+                                </View>
+                                <View style={{backgroundColor: 'blue', flex: 3}}>
+                                    <Text>North Desc</Text>
+                                </View>
+                            </View>
+                            <View style={{flexDirection: 'row'}}>
+                                <View style={{backgroundColor: 'green', flex: 1}}>
+                                    <Text>South</Text>
+                                </View>
+                                <View style={{backgroundColor: 'blue', flex: 3}}>
+                                    <Text>South Desc</Text>
+                                </View>
+                            </View>
+                        </View>
+                        
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{backgroundColor: 'green', flex: 1}}>
+                                <Text>Location</Text>
+                            </View>
+                            <View style={{backgroundColor: 'blue', flex: 3}}>
+                                <Text>Location Desc</Text>
+                            </View>
+                        </View>
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{backgroundColor: 'green', flex: 1}}>
+                                <Text>Rarity</Text>
+                            </View>
+                            <View style={{backgroundColor: 'blue', flex: 3}}>
+                                <Text>Rarity Desc</Text>
+                            </View>
+                        </View>
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{backgroundColor: 'green', flex: 1}}>
+                                <Text>Price</Text>
+                            </View>
+                            <View style={{backgroundColor: 'blue', flex: 3}}>
+                                <Text>Price Desc</Text>
+                            </View>
+                        </View>
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{backgroundColor: 'green', flex: 1}}>
+                                <Text>Flick Price</Text>
+                            </View>
+                            <View style={{backgroundColor: 'blue', flex: 3}}>
+                                <Text>Flick Price Desc</Text>
+                            </View>
+                        </View>
+                        <View style={{flexDirection: 'column'}}>
+                            <View style={{backgroundColor: 'green', flex: 1}}>
+                                <Text>Catchphrase</Text>
+                            </View>
+                            <View style={{backgroundColor: 'blue', flex: 2}}>
+                                <Text>Your catchphrase</Text>
+                            </View>
+                        </View>
+                        <View style={{flexDirection: 'column'}}>
+                            <View style={{backgroundColor: 'green', flex: 1}}>
+                                <Text>Blathers Catchphrase</Text>
+                            </View>
+                            <View style={{backgroundColor: 'blue', flex: 3}}>
+                                <Text>Blatherss catchphrase</Text>
+                            </View>
+                        </View>
+                    </View>
+
+
+
+            </View>
         );
     }
 
